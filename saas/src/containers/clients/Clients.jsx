82f868ab008934fd2d2b1ee35c95ace5c6bb3582
dvegaa20 <diego.vega.camacho@gmail.com>
2024-01-14 21:06:41 -0600

    implemented hero section

--- saas/src/containers/clients/Clients.jsx
+++ saas/src/containers/clients/Clients.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './clients.css'
+import { Patreon, Airbnb, Fiberplane, Coinbase, Griffin, Help_Scout, Plaid } from '../../assets'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <div className='saas__clients'>
+      <div className='saas__clients-images'>
+        <img src={Patreon} alt='patreon' href='https://www.patreon.com/' />
+        <img src={Airbnb} alt='airbnb' href='https://www.airbnb.mx/' />
+        <img src={Fiberplane} alt='fiberplane' href='' />
+        <img src={Coinbase} alt='coinbase' href='https://www.coinbase.com/es' />
+        <img src={Griffin} alt='griffin' href='' />
+        <img src={Help_Scout} alt='help_scout' href='https://www.helpscout.com/' />
+        <img src={Plaid} alt='plaid' href='https://plaid.com/' />
+      </div>
+    </div>
   )
 }
 
--- saas/src/containers/hero/Hero.jsx
+++ saas/src/containers/hero/Hero.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './hero.css'
+import tree from '../../assets/img_00.svg'
+import apps from '../../assets/img_01.svg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='saas__hero section__padding'>
+      <div className='saas__hero-content'>
+        <h1>Create, inspect, and apply synthetic surveillance broadly.</h1>
+        <p>Start with a stunning homepage. Stay motivated without hurting your pocket.</p>
+        <button type='button'>Start for free</button>
+        <div className='saas__hero-content__quickstart'>
+          <p>Want to talk or get a live demo? <strong>Get in touch →</strong></p>
+        </div>
+        {/* <div className='saas__hero-content__img-container'>
+          <img src={tree} className='saas__hero__img01'/>
+          <img src={apps} className='saas__hero__img00'/>
+        </div> */}
+      </div>
+    </div>
   )
 }
 
