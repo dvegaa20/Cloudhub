2de812fe2ec5e838c03a06877b8f77d4c489a729
dvegaa20 <diego.vega.camacho@gmail.com>
2024-01-17 13:29:18 -0600

    added first section for blog

--- saas/src/containers/blog/Blog.jsx
+++ saas/src/containers/blog/Blog.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './blog.css'
+import Projects from '../../assets/projects.svg'
+import Member from '../../assets/member.svg'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+      <div className='saas__blog-container'>
+        <div className='saas__blog-header'>
+          <h2>Your Archive of progress</h2>
+        </div>
+        <div className='saas__blog-content'>
+          <div className='saas__blog-content_projects'>
+            <div className='saas__blog-content_projects-header'>
+              <h4>Projects</h4>
+              <p>Organize, follow, and archive.</p>
+            </div>
+            <div className='saas__blog-content_projects-image'>
+              <img src={Projects} alt="" />
+            </div>
+          </div>
+          <div className='saas__blog-content_members'>
+            <div className='saas__blog-content_members-header'>
+              <h4>Member profiles</h4>
+              <p>Automatic portfolios of your work.</p>
+            </div>
+            <div className='saas__blog-content_members-image'>
+              <img src={Member} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
