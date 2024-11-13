29a592ab0a7b2ac655c390d46711b585cf317f0f
dvegaa20 <diego.vega.camacho@gmail.com>
2024-11-12 23:22:56 -0600

    feat(hero): finished image structure

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import './hero.css'
-import tree from '../../assets/img_00.svg'
-import apps from '../../assets/img_01.svg'
+import React from "react";
+import "./hero.css";
+import tree from "../../assets/img_00.svg";
+import apps from "../../assets/img_01.svg";
 
 const Hero = () => {
   return (
-    <div className='saas__hero section__padding'>
-      <div className='saas__hero-content'>
+    <div class="saas__hero section__padding">
+      <div class="saas__hero-content">
         <h1>Create, inspect, and apply synthetic surveillance broadly.</h1>
-        <p>Start with a stunning homepage. Stay motivated without hurting your pocket.</p>
-        <button type='button'>Start for free</button>
-        <div className='saas__hero-content__quickstart'>
-          <p>Want to talk or get a live demo? <strong>Get in touch →</strong></p>
-        </div>
-        {/* <div className='saas__hero-content__img-container'>
-          <img src={tree} className='saas__hero__img01'/>
-          <img src={apps} className='saas__hero__img00'/>
-        </div> */}
+        <p>
+          Start with a stunning homepage. Stay motivated without hurting your
+          pocket.
+        </p>
+        <button type="button">Start for free</button>
+        <p href="#" class="contact-link">
+          Want to talk or get a live demo? Get in touch →
+        </p>
+      </div>
+      <div class="image-overlay-container">
+        <img src={tree} alt="Árbol de líneas curvas" class="tree-image" />
+        <img src={apps} alt="Iconos de apps" class="icons-image" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
